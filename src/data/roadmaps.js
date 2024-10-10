8fa0851425538439dc0988d037bcb8502ed4c63f
Utkarsh <charmedtobe@gmail.com>
2024-10-10 12:45:21 -0400

    added more roadmaps

--- src/data/roadmaps.js
+++ src/data/roadmaps.js
@@ -8,7 +8,48 @@ const roadmaps = {
       Will: "low",
       Attack: "moderate",
       "Attk Damage": "extreme"
+    },
+    "Magical Striker": {
+      Attack: "high",
+      "Attk Damage": "high",
+      Perception: "moderate",
+      HP: "low",
+      "Save DC": "high",
+      Will: "high",
+      Fort: "low",
+      AC: "low",
+      Ref: "moderate"
+    },
+    "Skirmisher": {
+      Attack: "high",
+      Perception: "high",
+      Ref: "high",
+      Skill: "high",
+      "Attk Damage": "moderate",
+      HP: "moderate",
+      AC: "moderate",
+      Fort: "low"
+    },
+    "Sniper": {
+      Perception: "high",
+      Ref: "high",
+      Attack: "high",
+      "Attk Damage": "high",
+      Will: "low",
+      HP: "low",
+      AC: "low",
+      Fort: "terrible"
+    },
+    "Soldier": {
+      AC: "high",
+      Fort: "high",
+      Attack: "high",
+      "Attk Damage": "high",
+      Will: "moderate",
+      Ref: "low",
+      Perception: "moderate",
+      HP: "moderate"
     }
-    };
-    
+  };
+  
   export default roadmaps;
