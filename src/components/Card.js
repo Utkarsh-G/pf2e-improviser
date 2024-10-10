6778a515a588cdb2b0d6297ea13b9bf7c51964f1
Utkarsh <charmedtobe@gmail.com>
2024-10-10 11:18:24 -0400

    non functional roll button

--- src/components/Card.js
+++ src/components/Card.js
@@ -40,44 +40,47 @@ const Card = ({ name, level, baseTemplate }) => {
       <p>Level: {level}</p>
       <span>{baseTemplate}</span>
       <div className="stat-buttons-container">
-        {statButtons.map((stat, index) => (
-          <React.Fragment key={stat}>
-            {index === 2 && (
-              <input
-                type="text"
-                value={smallTextBox}
-                onChange={(e) => setSmallTextBox(e.target.value)}
-                maxLength="4"
-                placeholder="HP"
-                className="small-text-box"
-              />
+      {statButtons.map((stat, index) => (
+        <React.Fragment key={stat}>
+          {index === 2 && (
+            <input
+              type="text"
+              value={smallTextBox}
+              onChange={(e) => setSmallTextBox(e.target.value)}
+              maxLength="4"
+              placeholder="HP"
+              className="small-text-box"
+            />
+          )}
+          <div className="stat-button-container">
+            <button
+              className="stat-button"
+              onClick={() => handleButtonClick(stat)}
+              data-selected={selectedValues[stat] ? selectedValues[stat].option.toLowerCase() : ''}
+            >
+              {selectedValues[stat] 
+                ? `${stat}: ${selectedValues[stat].value} (${selectedValues[stat].option}) ` 
+                : stat}
+            </button>
+            {['Perception', 'Fort', 'Ref', 'Will', 'Attack', 'Skill'].includes(stat) && (
+              <button className="small-action-button">+</button>
             )}
-            <div className="stat-button-container">
-              <button
-                className="stat-button"
-                onClick={() => handleButtonClick(stat)}
-                data-selected={selectedValues[stat] ? selectedValues[stat].option.toLowerCase() : ''}
-              >
-                {selectedValues[stat] 
-                  ? `${stat}: ${selectedValues[stat].value} (${selectedValues[stat].option}) ` 
-                  : stat}
-              </button>
-              {showOptions[stat] && (
-                <div className="stat-options">
-                  {statCategories[stat].map((option) => (
-                    <button
-                      key={option}
-                      className={`stat-option ${option.toLowerCase()}`}
-                      onClick={() => handleOptionClick(stat, option)}
-                    >
-                      {option}
-                    </button>
-                  ))}
-                </div>
-              )}
-            </div>
-          </React.Fragment>
-        ))}
+            {showOptions[stat] && (
+              <div className="stat-options">
+                {statCategories[stat].map((option) => (
+                  <button
+                    key={option}
+                    className={`stat-option ${option.toLowerCase()}`}
+                    onClick={() => handleOptionClick(stat, option)}
+                  >
+                    {option}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
+        </React.Fragment>
+      ))}
       </div>
       <textarea
         value={largeTextBox}
